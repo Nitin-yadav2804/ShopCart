b77375b32c65096b1bc2fd0011a8a79b6d25b1a8
Nitin Yadav <nitinyadavsapla1@gmail.com>
2025-08-19 17:20:39 +0530

    Added .env

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
+const { PORT } = require('./config/serverConfig.js');
+
 const app = express();
 
 
 
-
-
-app.listen(3000, () => {
-    console.log(`Server for ShopCart is UP`);
+app.listen(PORT, () => {
+    console.log(`Server for ShopCart is UP on PORT ${PORT}`);
 })
