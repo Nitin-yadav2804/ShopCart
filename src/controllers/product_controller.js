27739156e42e74546b5c1f27e75f0bb642b692d8
Nitin Yadav <nitinyadavsapla1@gmail.com>
2025-08-20 00:03:27 +0530

    Added in memory db based service layer

--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -1,32 +1,63 @@
 const { StatusCodes, ReasonPhrases } = require('http-status-codes')
 
+const ProductService = require('../services/product_services.js')
+
 function createProduct(req, res) {
 
     try {
         
-        // some db processing
+        const response = ProductService.createProduct(req.body);
 
         return res
-            .status(StatusCodes.CREATED)
-            .json({
-                success: true,
-                error: {},
-                message: ReasonPhrases.CREATED + " Product",
-                data: {
-                    id: Math.random() * (20),
-                    title: req.body.title,
-                    description: req.body.description,
-                    category: req.body.category,
-                    price: req.body.price,
-                    image: req.body.image
-                }
-            })
+                .status(StatusCodes.CREATED)
+                .json({
+                    success: true,
+                    error: {},
+                    message: ReasonPhrases.CREATED + " Product",
+                    data: response
+         })
+    } catch (error) {
+        console.log("Something went wrong", error);
+    }
+}
+
+function getProducts(req, res) {
+    try {
+        const response = ProductService.getProducts();
+
+        return res
+                .status(StatusCodes.OK)
+                .json({
+                    success: true,
+                    error: {},
+                    message: "Successfully fetched the products",
+                    data: response
+         })
+    } catch (error) {
+        console.log("Something went wrong", error);
+    }
+}
+
+function getProduct(req,res) {
+    try {
+        const response = ProductService.getProduct(req.params.id);
+
+        return res
+                .status(StatusCodes.OK)
+                .json({
+                    success: true,
+                    error: {},
+                    message: "Successfully fetched the product",
+                    data: response
+         })
     } catch (error) {
         console.log("Something went wrong", error);
     }
 }
 
 module.exports = {
-    createProduct
+    createProduct,
+    getProducts,
+    getProduct
 }
  
--- src/routes/v1/product_router.js
+++ src/routes/v1/product_router.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { createProduct } = require("../../controllers/product_controller.js");
+const { createProduct, getProducts, getProduct } = require("../../controllers/product_controller.js");
 const { createProductValidator } = require('../../middlewares/product_middlewares.js')
 
 const productRouterV1 = express.Router();
 
-productRouterV1.get("/", createProduct);
 
 productRouterV1.post("/", createProductValidator, createProduct);
+productRouterV1.get("/", getProducts);
+productRouterV1.get("/:id", getProduct);
 
 module.exports = productRouterV1;
