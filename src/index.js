690c9d9ab22e8ed99207768342244e97c0b9230a
Nitin Yadav <nitinyadavsapla1@gmail.com>
2025-08-19 21:09:00 +0530

    Added Middlewares

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const responseTime = require('response-time');
 
 const { PORT } = require("./config/server_config.js");
+
 const ApiRouter = require("./routes/api_rouer.js");
+
 const app = express();
+
+// app.use(responseTime(function f(req, res, time){
+//     console.log("Time elasped = ", time);
+//     res.setHeader("X-Response-Time", time);
+// }));
+app.use(responseTime()); // This only provides time in header by default
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/api", ApiRouter);
 
--- src/routes/v1/ping_routerV1.js
+++ src/routes/v1/ping_routerV1.js
@@ -3,6 +3,26 @@ const { pingControllerV1 } = require("../../controllers/ping_controller.js");
 
 const pingRouterV1 = express.Router();
 
-pingRouterV1.get("/", pingControllerV1);
+
+function checkRequest(req, res, next) {
+    console.log("Checking the request");
+    next();
+    console.log("Ending Check Request");
+}
+
+function logger(req, res, next) { 
+    console.log("Logging Request");
+    next();
+    console.log("Ending Log Request");
+}
+
+function authChecker(req, res, next) {
+    console.log("Auth Checked");
+    next();
+    console.log("Ending Auth Request");
+}
+
+
+pingRouterV1.get("/", [checkRequest, logger, authChecker], pingControllerV1);
 
 module.exports = pingRouterV1;
